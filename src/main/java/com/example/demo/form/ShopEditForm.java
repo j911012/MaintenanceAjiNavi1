5cfee02544a467f0baa57352b85ee8af2c2c7578
jun <juntetsu.1012@gmail.com>
2026-09-08 22:58:18 +0900

    feat: 店舗編集用のShopEditForm作成

---
+++ src/main/java/com/example/demo/form/ShopEditForm.java
@@ -0,0 +1,19 @@
+package com.example.demo.form;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class ShopEditForm {
+	private Integer restaurantId;
+	
+	@NotNull(message="入力してください。")
+	@Size(min=1, max=32, message="1文字から32文字で指定してください。")
+	private String restaurantName;
+	
+	@NotNull(message="入力してください。")
+	@Size(min=1, max=64, message="1文字から64文字で指定してください。")
+	private String catchPhrase;
+}
